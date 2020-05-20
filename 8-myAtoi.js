e565ac7758e52eadb53f79e6f3afe66ee8a0efc3
SheldonLaw <sheldonlaw95@gmail.com>
2020-05-20 17:43:16 +0800

    update(8): 字符串转整数

---
+++ 8-myAtoi.js
@@ -0,0 +1,55 @@
+/**
+ * @param {string} str
+ * @return {number}
+ */
+
+// 1. 万能的修修补补大法（ifelse）
+var myAtoi = function(str) {
+    const length = str.length;
+    const r = [];
+    const max = Math.pow(2, 31) - 1;
+    const min = - (max + 1);
+    const charReg = /[+\-0-9]/;
+    let hasSymbol = false;
+    for(let i=0; i<length; i++) {
+        const char = str[i];
+        if (char === '+' || char === '-') {
+            // 符号重复 or 已有值后面出现符号，直接忽略后面的值
+            if (hasSymbol || r.length != 0) { break; }
+            else { hasSymbol = true; }
+        }
+        if (charReg.test(char)) {
+            r.push(char);
+        } else if (r.length !== 0) { // 前面有值的情况下，遇到不符合的字符，则忽略后面所有字符
+            break;
+        } else if (char !== ' ') { // 前面没值，遇到非法字符（非空），返回0
+            return 0;
+        }
+    }
+    // return parseInt(r.join('')); // 这是作弊
+    const compute = (arr, isNegative) => {
+        let pow = isNegative ? -1 : 1;
+        let sum = 0;
+        for (let i=arr.length - 1; i>=0; i--) {
+            sum += arr[i] * pow;
+            if (sum > max) { return max; }
+            if (sum < min) { return min; }
+            pow = pow * 10;
+        }
+        return sum;
+    };
+    if (r[0] === '-') {
+        return compute(r.slice(1), true);
+    } else if (r[0] === '+') {
+        return compute(r.slice(1), false);
+    } else {
+        return compute(r, false);
+    }
+};
+
+// 2. 高冷的自动机算法
+var myAtoi = function(str) {
+    
+}
+
+console.log(myAtoi("  +  413"));
